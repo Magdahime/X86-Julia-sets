fea911b35fb188fef541bd412ba8949a0249e79b
magdahime <magdalena.majkowska1@gmail.com>
2019-12-30 19:24:27 +0100

    Delete drawing text --> too much work, add pixel array and displaying it

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
-#include <SFML/Audio.hpp>
+#include <sstream>
 #include <SFML/Graphics.hpp>
-std::string create_title(double imaginary_part, double real_part);
+#include <SFML/Window.hpp>
+
+//HELP FUNCTIONS
 int main(int argc, char* argv[])
 {
     unsigned int width, height;
@@ -13,15 +15,37 @@ int main(int argc, char* argv[])
         width = 400;
         height = 400;
     }
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(width, height), "Julia sets");
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // CREATING THE MAIN WINDOW
+    sf::RenderWindow window(sf::VideoMode(width, height,32), "Julia sets",sf::Style::Default);
+    sf::Image image;
     sf::Texture texture;
-    sf::Sprite sprite(texture);
+    sf::Sprite sprite;
+    sf::Uint8 *pixels = new unsigned char[width * height * 4];
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     double real_part = 0;
     double imaginary_part = 0;
     double * current = &real_part;
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    std::cout<<"***********************************************************************************"<<std::endl;
+    std::cout<<"For help press 'H'"<<std::endl;
+    std::cout<<"***********************************************************************************"<<std::endl;
+
+        for(int x = 0 ; x < width; x++){
+            for(int y = 0; y < height; y++){
+                pixels[(y*width + x)*4] =      y;
+                pixels[(y*width + x)*4 + 1 ] = x;
+                pixels[(y*width + x)*4 + 2 ] = 0;
+                pixels[(y*width + x)*4 + 3 ] = 255;
+            }
+        }
+
     while (window.isOpen())
-    {
+    {   
         // Process events
         sf::Event event;
         while (window.pollEvent(event))
@@ -37,48 +61,55 @@ int main(int argc, char* argv[])
                 std::cout << "mouse y: " << event.mouseWheelScroll.y << std::endl;
             }
             if (event.type == sf::Event::KeyPressed){
-                    std::cout<<"\n\n\n\n\n"<<std::endl;
-                    std::cout<<"***********************************************************************************"<<std::endl;
-                    std::cout<<"Current value of imaginary part:"<<imaginary_part<<std::endl;
-                    std::cout<<"Current value of real part:"<<real_part<<std::endl;
-                    std::cout<<"For help press 'H'"<<std::endl;
-                    std::cout<<"***********************************************************************************"<<std::endl;
+                std::cout<<"\n\n***********************************************************************************"<<std::endl;
+                std::cout<<"Current value of imaginary part: "<<imaginary_part<<std::endl;
+                std::cout<<"Current value of real part: "<<real_part<<std::endl;
+                std::cout<<"For help press 'H'"<<std::endl;
+                std::cout<<"***********************************************************************************"<<std::endl;
+
                     if(event.key.code == sf::Keyboard::H){
+
                         std::cout<<"\n\n\n\n***********************************************************************************"<<std::endl;
                         std::cout<<"To switch between imaginary and real part ----> Arrow Right/Left"<<std::endl;
                         std::cout<<"To increase the value of chosen part -----> Arrow Up"<<std::endl;
                         std::cout<<"To decrease the value of chosen part -----> Arrow Down"<<std::endl;
                         std::cout<<"To zoom in/out the picture ----> Scroll with mouse"<<std::endl;
                         std::cout<<"***********************************************************************************"<<std::endl;
+
                     }else if(event.key.code == sf::Keyboard::Right){
-                        std::cout<<"Changing value of imaginary part."<<std::endl;
+
+                        std::cout<<"\n\nChanging value of imaginary part."<<std::endl;
                         current = &imaginary_part;
+
                     }else if(event.key.code == sf::Keyboard::Left){
-                        std::cout<<"Changing value of real part"<<std::endl;
+
+                        std::cout<<"\n\nChanging value of real part"<<std::endl;
                         current = &real_part;
+
                     }else if(event.key.code == sf::Keyboard::Up){
+
                         *current+=0.01;
+                    
                     }else if(event.key.code == sf::Keyboard::Down){
+                    
                         *current-=0.01;
+                    
                     }
             }
         }
-        // Clear screen
-        const std::string new_title(create_title(imaginary_part,real_part));
+
+        //CREATING THE OUTPUT FROM THE ARRAY OF PIXELS
+        image.create(width,height,pixels);
+        texture.create(width,height);
+        texture.update(image);
+        sprite.setTexture(texture);
+        //CLEARS THE SCREEN
         window.clear();
-        window.setTitle(new_title);
         // Draw the sprite
         window.draw(sprite);
         // Update the window
         window.display();
     }
-    return EXIT_SUCCESS;
-}
-std::string create_title(double imaginary_part, double real_part){
-    std::string title="JULIA SETS: ";
-    title+="IM: ";
-    title+= std::to_string(imaginary_part);
-    title+="RE: ";
-    title+= std::to_string(real_part);
-    return title;
+    delete [] pixels;
+    return 0;
 }
